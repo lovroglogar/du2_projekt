ffd38909a3b930d3de2bc5322548e7fa4d5b75f7
Mislav Perić <mislav.peric7@gmail.com>
2023-12-30 18:24:20 +0100

    add networks and training

---
+++ networks.py
@@ -0,0 +1,197 @@
+import torch
+import torch.nn as nn
+
+class ConvolutionalBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        is_downsampling: bool = True,
+        add_activation: bool = True,
+        **kwargs
+    ):
+        super().__init__()
+        if is_downsampling:
+            self.conv = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, padding_mode="reflect", **kwargs),
+                nn.InstanceNorm2d(out_channels),
+                nn.ReLU(inplace=True) if add_activation else nn.Identity(),
+            )
+        else:
+            self.conv = nn.Sequential(
+                nn.ConvTranspose2d(in_channels, out_channels, **kwargs),
+                nn.InstanceNorm2d(out_channels),
+                nn.ReLU(inplace=True) if add_activation else nn.Identity(),
+            )
+
+    def forward(self, x):
+        return self.conv(x)
+class ResidualBlock(nn.Module):
+    def __init__(self, channels: int):
+        super().__init__()
+        self.block = nn.Sequential(
+            ConvolutionalBlock(channels, channels, add_activation=True, kernel_size=3, padding=1),
+            ConvolutionalBlock(channels, channels, add_activation=False, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+class Generator(nn.Module):
+    def __init__(
+        self, img_channels: int, num_features: int = 64, num_residuals: int = 6
+    ):
+        """
+        Generator consists of 2 layers of downsampling/encoding layer, 
+        followed by 6 residual blocks for 128 × 128 training images 
+        and then 3 upsampling/decoding layer. 
+        
+        The network with 6 residual blocks can be written as: 
+        c7s1–64, d128, d256, R256, R256, R256, R256, R256, R256, u128, u64, and c7s1–3.
+        """
+        super().__init__()
+        self.initial_layer = nn.Sequential(
+            nn.Conv2d(
+                img_channels,
+                num_features,
+                kernel_size=7,
+                stride=1,
+                padding=3,
+                padding_mode="reflect",
+            ),
+            nn.InstanceNorm2d(num_features),
+            nn.ReLU(inplace=True),
+        )
+
+        self.downsampling_layers = nn.ModuleList(
+            [
+                ConvolutionalBlock(
+                    num_features, 
+                    num_features * 2,
+                    is_downsampling=True, 
+                    kernel_size=3, 
+                    stride=2, 
+                    padding=1,
+                ),
+                ConvolutionalBlock(
+                    num_features * 2,
+                    num_features * 4,
+                    is_downsampling=True,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                ),
+            ]
+        )
+
+        self.residual_layers = nn.Sequential(
+            *[ResidualBlock(num_features * 4) for _ in range(num_residuals)]
+        )
+
+        self.upsampling_layers = nn.ModuleList(
+            [
+                ConvolutionalBlock(
+                    num_features * 4,
+                    num_features * 2,
+                    is_downsampling=False,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                    output_padding=1,
+                ),
+                ConvolutionalBlock(
+                    num_features * 2,
+                    num_features * 1,
+                    is_downsampling=False,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                    output_padding=1,
+                ),
+            ]
+        )
+
+        self.last_layer = nn.Conv2d(
+            num_features * 1,
+            img_channels,
+            kernel_size=7,
+            stride=1,
+            padding=3,
+            padding_mode="reflect",
+        )
+
+    def forward(self, x):
+        x = self.initial_layer(x)
+        for layer in self.downsampling_layers:
+            x = layer(x)
+        x = self.residual_layers(x)
+        for layer in self.upsampling_layers:
+            x = layer(x)
+        return torch.tanh(self.last_layer(x))
+class ConvInstanceNormLeakyReLUBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, stride: int):
+        """
+        Class object initialization for Convolution-InstanceNorm-LeakyReLU layer
+
+        We use leaky ReLUs with a slope of 0.2.
+        """
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=4,
+                stride=stride,
+                padding=1,
+                bias=True,
+                padding_mode="reflect",
+            ),
+            nn.InstanceNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+        
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3, features=[64, 128, 256, 512]):
+        super().__init__()
+        self.initial_layer = nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                features[0],
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                padding_mode="reflect",
+            ),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(
+                ConvInstanceNormLeakyReLUBlock(
+                    in_channels, 
+                    feature, 
+                    stride=1 if feature == features[-1] else 2,
+                )
+            )
+            in_channels = feature
+ 
+        layers.append(
+            nn.Conv2d(
+                in_channels,
+                1,
+                kernel_size=4,
+                stride=1,
+                padding=1,
+                padding_mode="reflect",
+            )
+        )
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.initial_layer(x)
+
+        return torch.sigmoid(self.model(x))
